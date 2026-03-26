68a2fcc829d5dec85d3998cf04a0309eb7c510d8
Andy Hayward <ach@buteo.org>
2026-03-26 10:54:26 -0600

    implement configuration reading and user setting in main function

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/ach1000/gator/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cfg.SetUser("andy")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", cfg)
 }
